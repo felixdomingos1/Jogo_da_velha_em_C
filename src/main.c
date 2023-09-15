06ee3c3678a1ee843b7ea9f3ca7c031828d37bb9
felixdomingos1 <felixsdomingos93@gmail.com>
2023-09-15 01:50:11 +0100

    add the func checkWinner

--- src/main.c
+++ src/main.c
@@ -120,7 +120,32 @@ void computerMove()
 
 char checkWinner()
 {
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+		{
+			return board[i][0];
+		}
+		
+	}
 
+	for (int j = 0; j < 3; j++)
+	{
+		if (board[0][j] == board[1][j] && board[0][j] == board[2][j])
+		{
+			return board[0][j];
+		}
+		
+	}
+	if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+	{
+		return board[0][0];
+	}
+	if (board[0][2] == board[1][1] && board[0][0] == board[2][0])
+	{
+		return board[0][2];
+	}
+	return ' ';
 }
 
 
